8a823f258a75760f7d79bcba7eda0dccce3e58c4
ddavejjackson <protoblaster11@outlook.ie>
2026-03-13 15:03:08 +0000

    update navigation BaseLayout and menu.js

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,12 @@
-const btn = document.getElementById("menu-btn");
-const menu = document.getElementById("mobile-menu");
+  const btn = document.getElementById("menu-btn");
+  const menu = document.getElementById("mobile-menu");
 
-btn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-});
+  btn.addEventListener("click", () => {
+    if (menu.classList.contains("max-h-0")) {
+      menu.classList.remove("max-h-0", "opacity-0");
+      menu.classList.add("max-h-96", "opacity-100", "pb-4");
+    } else {
+      menu.classList.add("max-h-0", "opacity-0");
+      menu.classList.remove("max-h-96", "opacity-100", "pb-4");
+    }
+  });
